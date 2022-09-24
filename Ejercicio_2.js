84a20ce4f4b1d4572fa20c82eef3cc35ff44c906
chichia555 <chia555@hotmail.com>
2022-09-24 19:44:45 -0300

    Ejercicio.2

---
+++ Ejercicio_2.js
@@ -0,0 +1,11 @@
+const lista = [
+    "Cecilia", 
+    37, 
+    true, 
+    new date (1985,03,29),
+    {nombre:"El inversor inteligente",
+    autor:"Benjamin Graham", 
+    fecha: new date (1949),
+    url: "https://books.google.com.ar/books/about/El_inversor_inteligente.html?id=MOrpQjL8OkEC&printsec=frontcover&source=kp_read_button&hl=es&redir_esc=y#v=onepage&q&f=false"}
+]
+console.log(lista);
